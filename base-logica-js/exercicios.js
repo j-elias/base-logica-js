e2877417141a333d48750329655f7a8fa9776c61
Jose Elias <j.elias.silva00@gmail.com>
2026-02-13 20:26:58 -0300

    adicionado mais exercicios no arquivo

--- base-logica-js/exercicios.js
+++ base-logica-js/exercicios.js
@@ -21,7 +21,7 @@ console.log(fatorial_iterativo(5));
 
 function maior_numero(array) {
     let maior = array[0];
-    for(i=0;i< array.length; i++) {
+    for(let i=0; i< array.length; i++) {
         if(array[i] > maior) {
             maior = array[i];
         }
@@ -43,3 +43,53 @@ function palindromo(palavra) {
 }
 
 console.log(palindromo('arara'));
+
+
+function remove_duplicados(array) {
+    let array_resposta = [];
+    for(let i = 0; i < array.length; i++)
+    {
+        if (i == 0) {
+            array_resposta.push(array[i]);
+        }
+        else {
+            if (!array_resposta.includes(array[i])) {
+                array_resposta.push(array[i]);
+            }
+        }
+    }
+    return array_resposta;
+}
+
+console.log(remove_duplicados([1, 2, 3, 2, 4, 1, 5]));
+
+function busca_linear(array, valor) {
+    for(let i = 0; i < array.length; i++) {
+        if (array[i] == valor) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+console.log(busca_linear([1, 2, 3, 4, 5], 3));
+
+function busca_binaria(array, valor) {
+    let inicio = 0;
+    let fim = array.length - 1;
+    while (inicio <= fim) {
+        let meio = Math.floor((inicio + fim) / 2);
+        if (array[meio] === valor) {
+            return meio;
+        }
+        else if (array[meio] < valor) {
+            inicio = meio + 1;
+        }
+        else {
+            fim = meio - 1;
+        }
+    }
+    return -1;
+}
+console.log(busca_binaria([1, 2, 3, 4, 5], 3));
+    
